82ea1471b1481793727cade5e6c0dda742a2acd5
Vera104102007 <hotfloe0410@gmail.com>
2026-03-10 12:37:51 +0300

    Add files via upload

---
+++ тема1/тема1/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(273, 163);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите значение x (угол в радианах):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(701, 163);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите точность (epsilon):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(272, 207);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(705, 207);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 26);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(478, 244);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 70);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ВЫЧИСЛИТЬ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::тема1.Properties.Resources.Новый_точечный_рисунок;
+            this.pictureBox1.Location = new System.Drawing.Point(272, 31);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(675, 101);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 385);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Sin(x) =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(273, 445);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Сумма ряда:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(273, 505);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(228, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Количество членов ряда:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(277, 571);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(1333, 692);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Вычисление гиперболического косинуса (ch x) через ряд";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ тема1/тема1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace project1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Обработчик KeyPress для textBox1 (ввод x)
+        /// </summary>
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                e.KeyChar != ',' && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }
+
+            if (e.KeyChar == ',' && (sender as TextBox).Text.Contains(','))
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Обработчик KeyPress для textBox2 (ввод точности)
+        /// </summary>
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                e.KeyChar != ',' && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }
+
+            if (e.KeyChar == ',' && (sender as TextBox).Text.Contains(','))
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Обработчик TextChanged для textBox1
+        /// </summary>
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = !string.IsNullOrWhiteSpace(textBox1.Text) &&
+                              !string.IsNullOrWhiteSpace(textBox2.Text);
+        }
+
+        /// <summary>
+        /// Обработчик TextChanged для textBox2
+        /// </summary>
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = !string.IsNullOrWhiteSpace(textBox1.Text) &&
+                              !string.IsNullOrWhiteSpace(textBox2.Text);
+        }
+
+        /// <summary>
+        /// Обработчик Click для button1 - вычисление sin(x) через ряд
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label6.Text = ""; // Очистка сообщения об ошибке
+
+            try
+            {
+                // Ввод данных
+                double x = Convert.ToDouble(textBox1.Text);
+                double eps = Convert.ToDouble(textBox2.Text);
+
+                // Проверка точности
+                if (eps <= 0)
+                {
+                    label6.Text = "Ошибка: Точность должна быть положительным числом";
+                    return;
+                }
+
+                // Эталонное значение для проверки
+                double mathResult = Math.Sin(x);
+
+                // Вычисляем сумму в скобках S = 1 - x²/3! + x⁴/5! - x⁶/7! + ...
+                double sumInBrackets = 0.0;    // Сумма в скобках
+                double term = 1.0;              // Первый член в скобках (1)
+                int n = 0;                       // Индекс члена (n=0 для первого члена 1)
+                int sign = 1;                     // Знак (+1 или -1)
+                int count = 0;                     // Счетчик членов в скобках
+
+                // Вычисляем члены ряда в скобках, пока текущий член больше точности
+                do
+                {
+                    // Добавляем текущий член с соответствующим знаком
+                    sumInBrackets += sign * term;
+                    count++;
+
+                    // Вычисляем следующий член: term(n+1) = term(n) * x² / ((2n+2)*(2n+3))
+                    // Для n=0: term1 = 1 * x² / (2*3) = x²/6 (это x²/3! = x²/6)
+                    // Для n=1: term2 = (x²/6) * x² / (4*5) = x⁴/120 (это x⁴/5!)
+                    double nextTerm = term * (x * x) / ((2 * n + 2) * (2 * n + 3));
+
+                    term = nextTerm;
+                    n++;
+                    sign = -sign; // Меняем знак для следующего члена
+
+                } while (Math.Abs(term) > eps);
+
+                // Умножаем сумму в скобках на x
+                double seriesResult = x * sumInBrackets;
+
+                // Вывод результатов
+                label3.Text = $"Math.Sin({x:F3}) = {mathResult:F8}";
+                label4.Text = $"Сумма ряда = {seriesResult:F8}";
+                label5.Text = $"Количество членов = {count}";
+            }
+            catch (FormatException)
+            {
+                label6.Text = "Ошибка: Используйте запятую для дробных чисел";
+            }
+            catch (Exception ex)
+            {
+                label6.Text = $"Ошибка: {ex.Message}";
+            }
+        }
+    }
+}
