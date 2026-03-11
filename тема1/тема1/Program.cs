6ec79306a1830d00e7fe5d64546f2362718bda91
Vera Smelyanskaya <Hotfloe0410@gmail.com>
2026-03-11 10:40:57 +0300

    from vs

--- тема1/тема1/Program.cs
+++ тема1/тема1/Program.cs
@@ -14,7 +14,7 @@ namespace project1
         /// hello
         [STAThread]
         static void Main()
-        {//!!!!!!!!!!!!
+        {//!!!!!!!!!!!!!!!!!
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
