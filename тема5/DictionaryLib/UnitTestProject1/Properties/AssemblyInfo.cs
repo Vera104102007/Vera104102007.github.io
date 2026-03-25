bfed44aa543448583a4c360b8fc4e950d7d3f2de
Hotfloe <Hotfloe0410@gmail.com>
2026-03-25 04:29:57 +0300

    отчет с тестами

---
+++ тема5/DictionaryLib/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b0401aca-f5b9-4b17-b6fc-be3015582def")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ тема5/DictionaryLib/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DictionaryLib;
+using System.Collections.Generic;
+
+namespace DictionaryLib.Tests
+{
+    [TestClass]
+    public class SlovarTests
+    {
+        private Slovar _slovar;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Инициализация перед каждым тестом
+            _slovar = new Slovar();
+            _slovar.AddWord("молоко");
+            _slovar.AddWord("мама");
+            _slovar.AddWord("компьютер");
+        }
+
+        [TestMethod]
+        public void TestAddWord()
+        {
+            _slovar.AddWord("тест");
+            Assert.AreEqual(4, _slovar.Count);
+        }
+
+        [TestMethod]
+        public void TestExactSearch()
+        {
+            var results = _slovar.ExactSearch("молоко");
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual("молоко", results[0]);
+        }
+
+        [TestMethod]
+        public void TestLevenshteinSearch_WithError()
+        {
+            // Проверка: найдет ли "молоко", если ввели "малако" (1 правка)
+            var results = _slovar.LevenshteinSearch("малако", 2);
+            Assert.IsTrue(results.Contains("молоко"));
+        }
+
+        [TestMethod]
+        public void TestSearchVariant4_ByLengthAndPart()
+        {
+            // Ищем слово длиной 4, которое начинается/заканчивается на "ма"
+            var results = _slovar.SearchVariant4(4, "ма");
+            Assert.IsTrue(results.Contains("мама"));
+        }
+
+        [TestMethod]
+        public void TestDeleteWord()
+        {
+            _slovar.DeleteWord("мама");
+            Assert.AreEqual(2, _slovar.Count);
+            Assert.IsFalse(_slovar.ExactSearch("мама").Count > 0);
+        }
+
+        [TestMethod]
+        public void TestClearAll()
+        {
+            _slovar.ClearAll();
+            Assert.AreEqual(0, _slovar.Count);
+        }
+    }
+}
